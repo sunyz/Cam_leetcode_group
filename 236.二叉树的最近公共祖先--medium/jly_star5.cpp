3b9a75297fe23f83fa8befe68555a8199465dc66
jinluyang <1055057679@qq.com>
2019-06-02 19:54:06 +0800

    Create jly_star5.cpp

---
+++ 236.二叉树的最近公共祖先--medium/jly_star5.cpp
@@ -0,0 +1,4 @@
+代码跟我235的一样，当时是按照不是搜索树做的
+结果波动也挺大，我第一次跑的时候才击败40%
+执行用时 : 20 ms, 在Lowest Common Ancestor of a Binary Tree的C++提交中击败了99.16% 的用户
+内存消耗 : 16.6 MB, 在Lowest Common Ancestor of a Binary Tree的C++提交中击败了70.56% 的用户
