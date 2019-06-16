5b5dbf7cf9c39e1fec23615c0fcd7c85459ca241
jinluyang <1055057679@qq.com>
2019-06-16 21:37:50 +0800

    Create jly_5star.cpp

---
+++ 231.2的幂/jly_5star.cpp
@@ -0,0 +1,32 @@
+/*
+给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+考察左移右移
+执行用时 :
+4 ms
+, 在所有C++提交中击败了
+92.33%
+的用户
+内存消耗 :
+8.3 MB
+, 在所有C++提交中击败了
+5.71%
+的用户
+*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int t = n;
+        if (n <= 0)
+            return false;
+        int count = 0;
+        while (t) {
+            t = t >> 1;
+            count++;
+        }
+        t = 1 << count-1;
+        if (t == n)
+            return true;
+        else
+            return false;
+    }
+};
