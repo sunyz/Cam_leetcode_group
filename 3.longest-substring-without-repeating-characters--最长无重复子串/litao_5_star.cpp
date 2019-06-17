e6a17f70243cc84db0c27fdb15947885177f6fe0
litao <362085095@qq.com>
2019-06-17 19:03:45 +0800

    [ID-003][star-5] 将map搜素修改成数据位搜索

--- 3.longest-substring-without-repeating-characters--最长无重复子串/litao_5_star.cpp
+++ 3.longest-substring-without-repeating-characters--最长无重复子串/litao_5_star.cpp
@@ -14,15 +14,41 @@
 // * 2. 若未出现则建立新的map对
 // * 3. 字符串长度为有边界减去左边界。
 
+// 方法一：
 // 执行用时 : 40 ms , 在所有C++提交中击败了 50.69% 的用户
 // 内存消耗 : 11 MB , 在所有C++提交中击败了 77.14% 的用户
+
+// 方法二：
+// 执行用时 : 8 ms , 在所有C++提交中击败了 98.85% 的用户
+// 内存消耗 : 9.3 MB , 在所有C++提交中击败了 86.91% 的用户
 
 #include <string>
 #include <iostream>
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <memory.h>
 using namespace std;
+
+int lengthOfLongestSubstring2(string s) {
+  int max = 0;
+  int left = 0;
+  int length = 0;
+  int char_num[256] = {-1};
+  memset(char_num, -1, sizeof(char_num));
+  for (int j = left; j < s.size(); j++) {
+    if (char_num[s[j]] == -1 || char_num[s[j]] < left) {
+      length = j - left + 1;
+    } else {
+      left = char_num[s[j]] + 1 ;
+    }
+    char_num[s[j]] = j;
+    if (length > max ) {
+      max = length;
+    }
+  }
+  return max;
+}
 
 int lengthOfLongestSubstring(string s) {
   int max = 0;
@@ -47,6 +73,6 @@ int lengthOfLongestSubstring(string s) {
 int main() {
   std::string str;
   cin >> str;
-  int max = lengthOfLongestSubstring(str);
+  int max = lengthOfLongestSubstring2(str);
   std::cout << max << endl;
 }
