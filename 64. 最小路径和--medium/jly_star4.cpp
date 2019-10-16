6c1aabe9c941839eb84fd377aefae8417e9288d8
jinluyang <1055057679@qq.com>
2019-10-16 20:19:09 +0800

    好像是看过这道题，才能做出来的，动态规划

---
+++ 64. 最小路径和--medium/jly_star4.cpp
@@ -0,0 +1,40 @@
+/*
+给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。
+
+示例:
+
+输入:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 7
+解释: 因为路径 1→3→1→1→1 的总和最小。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        std::vector<int> shortest(n);
+        shortest[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            shortest[i] = grid[0][i] + shortest[i-1];
+        }
+        // cout << shortest[n-1]<< endl;
+        for (int i = 1; i < m; i++) {
+            shortest[0] = shortest[0] + grid[i][0];
+            for (int j = 1; j < n; j++) {
+                shortest[j] = min(shortest[j-1], shortest[j]) + grid[i][j];
+            }
+        }
+        return shortest[n-1];
+    }
+};
